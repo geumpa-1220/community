c4b198f0480a8036b81c01fc5882bfaebb6f0710
Mac <mac@Macui-MacBookAir.local>
2024-09-08 14:31:24 +0900

    update

--- src/main/java/com/example/demo/dto/UserDto.java
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -11,5 +11,35 @@ public class UserDto {
 	private Timestamp usertime; 
 	
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public Timestamp getUsertime() {
+		return usertime;
+	}
+	public void setUsertime(Timestamp usertime) {
+		this.usertime = usertime;
+	}
 
 }
